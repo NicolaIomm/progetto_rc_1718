7d5f4e541a1843ca05045f20aa48d66774d9bd9b
NicolaIomm <alocinalocin@gmail.com>
2017-12-07 13:32:21 +0100

    completed server and created functions.py

---
+++ functions.py
@@ -0,0 +1,109 @@
+import http.server
+import http.client
+import urllib.parse
+import ssl
+import base64
+import json
+import time
+
+    # Crea contesto ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+    # Global variables used for OAuth 2.0 Spotify
+client_id = "0b0257f3ab104ffc89c6f4529161b19c"
+client_secret_key = "85644beebd884fadb72fd7f766ee9814"
+scope = urllib.parse.quote("user-follow-read", safe='')
+redirect_uri = "http://127.0.0.1:3000/callback"
+authorization_code = ""
+
+       # Ottengo url per accesso al proprio account spotify
+def get_authorize_url():
+    response_type = "code"
+    url = ("https://accounts.spotify.com/authorize/?client_id="+client_id+
+            "&response_type="+response_type+
+            "&redirect_uri="+urllib.parse.quote(redirect_uri, safe = '')+
+            "&scope="+scope)
+    return url
+
+    # Effettuo la richiesta per ottenere un token
+def do_token_request(request_handler, code):
+        # Ottengo header per effettuare la richiesta del token
+    def getheaders_tokenrequest():
+        def getauthorizationcode_tokenrequest():
+            info = client_id+":"+client_secret_key
+            base64code = base64.b64encode(info.encode())
+            authorization_code = "Basic " + base64code.decode()
+            return str(authorization_code)
+        authorization_code = getauthorizationcode_tokenrequest()
+        headers = { "Authorization":authorization_code,
+                    "Content-type":"application/x-www-form-urlencoded" }
+        return headers #urllib.parse.urlencode(headers)
+    
+        # Ottengo body per effettuare la richiesta del token
+    def getbody_tokenrequest(code,grant_type):
+        body = {"grant_type":grant_type,
+                "code":code,
+                "redirect_uri":redirect_uri}
+        return urllib.parse.urlencode(body)    
+
+        # retriving parameters for token_request
+    body_parameters = getbody_tokenrequest(code, "authorization_code")
+    headers = getheaders_tokenrequest()
+
+        # token_request
+    token_request = http.client.HTTPSConnection("accounts.spotify.com") 
+    token_request.request("POST",
+                          "/api/token",
+                          body_parameters.encode(),
+                          headers )
+    response = token_request.getresponse()
+    
+    request_handler.send_response(response.status, response.reason)
+    request_handler.end_headers()
+
+    json_response = json.loads(response.read().decode())
+    return json_response
+
+def do_refresh_token_request(request_handler, old_token):
+
+    body_parameters = {"grant_type":grant_type,
+                       "refresh_token":old_token}
+    
+    token_refresh_request = http.client.HTTPSConnection("account.spotify.com")
+    token_refresh_request.request("POST",
+                                  "/api/token",
+                                  body_parameters.encode(),
+                                  get_headers_token_request())
+    response = token_refresh_request.getresponse()
+                       
+    request_handler.send_response(response.status, response.reason)
+    request_handler.end_headers()
+
+    json_response = json.loads(response.read().decode())
+    return json_response                      
+
+    # Parse della risposta per ottenere il token
+def parse_json_response_token(json_response):
+    access_token = json_response["access_token"]
+    token_type = json_response["token_type"]
+    expires_in = int(json_response["expires_in"])
+    scope = json_response["scope"]
+    return (access_token, token_type, expires_in, scope)
+
+    # Effettuo la richiesta per ottenere gli artisti seguiti
+def do_followed_artists_query(access_token):
+    path = "/v1/me/following?type=artist"
+    followed_request = http.client.HTTPSConnection("api.spotify.com") 
+    followed_request.request("GET",
+                              path,
+                              None,
+                              {"Authorization":"Bearer "+access_token,
+                               #"Content-type":"application/x-www-form-urlencoded",
+                               "Connection":"keep-alive"})
+    return followed_request.getresponse() 
+
+def parse_json_response_artists(json_data):
+    names = []
+    for item in json_data["artists"]["items"]:
+        names.append(item["name"])
+    return names
--- startServer.py
+++ startServer.py
@@ -1,93 +1,4 @@
-import http.server
-import http.client
-import urllib.parse
-import ssl
-import base64
-import json
-
-    # Crea contesto ssl
-ssl._create_default_https_context = ssl._create_unverified_context
-
-    # Global variables used for OAuth 2.0 Spotify
-client_id = "0b0257f3ab104ffc89c6f4529161b19c"
-client_secret_key = "85644beebd884fadb72fd7f766ee9814"
-scope = urllib.parse.quote("user-follow-read", safe='')
-redirect_uri = "http://127.0.0.1:3000/callback"
-authorization_code = ""
-
-       # Ottengo url per accesso al proprio account spotify
-def get_authorize_url():
-    response_type = "code"
-    url = ("https://accounts.spotify.com/authorize/?client_id="+client_id+
-            "&response_type="+response_type+
-            "&redirect_uri="+urllib.parse.quote(redirect_uri, safe = '')+
-            "&scope="+scope)
-    return url
-
-    # Effettuo la richiesta per ottenere un token
-def do_token_request(request_handler, code):
-        # Ottengo header per effettuare la richiesta del token
-    def getheaders_tokenrequest():
-        def getauthorizationcode_tokenrequest():
-            info = client_id+":"+client_secret_key
-            base64code = base64.b64encode(info.encode())
-            authorization_code = "Basic " + base64code.decode()
-            return str(authorization_code)
-        authorization_code = getauthorizationcode_tokenrequest()
-        headers = { "Authorization":authorization_code,
-                    "Content-type":"application/x-www-form-urlencoded" }
-        return headers #urllib.parse.urlencode(headers)
-    
-        # Ottengo body per effettuare la richiesta del token
-    def getbody_tokenrequest(code,grant_type):
-        body = {"grant_type":grant_type,
-                "code":code,
-                "redirect_uri":redirect_uri}
-        return urllib.parse.urlencode(body)    
-
-        # retriving parameters for token_request
-    body_parameters = getbody_tokenrequest(code, "authorization_code")
-    headers = getheaders_tokenrequest()
-
-        # token_request
-    token_request = http.client.HTTPSConnection("accounts.spotify.com") 
-    token_request.request("POST",
-                          "/api/token",
-                          body_parameters.encode(),
-                          headers )
-    response = token_request.getresponse()
-    
-    request_handler.send_response(response.status, response.reason)
-    request_handler.end_headers()
-
-    json_response = json.loads(response.read().decode())
-    return json_response
-
-    # Parse della risposta per ottenere il token
-def parse_json_response_token(json_response):
-    access_token = json_response["access_token"]
-    token_type = json_response["token_type"]
-    expires_in = json_response["expires_in"]
-    scope = json_response["scope"]
-    return (access_token, token_type, expires_in, scope)
-
-    # Effettuo la richiesta per ottenere gli artisti seguiti
-def do_followed_artists_query(access_token):
-    path = "/v1/me/following?type=artist"
-    followed_request = http.client.HTTPSConnection("api.spotify.com") 
-    followed_request.request("GET",
-                              path,
-                              None,
-                              {"Authorization":"Bearer "+access_token,
-                               #"Content-type":"application/x-www-form-urlencoded",
-                               "Connection":"keep-alive"})
-    return followed_request.getresponse() 
-
-def parse_json_response_artists(json_data):
-    names = []
-    for item in json_data["artists"]["items"]:
-        names.append(item["name"])
-    return names
+from functions import *
 
     # Gestore richieste alle risorse del server localhost
 class myHttpRequestHandler(http.server.BaseHTTPRequestHandler):
@@ -112,14 +23,18 @@ class myHttpRequestHandler(http.server.BaseHTTPRequestHandler):
             #message = file.read()
             #self.wfile.write(message.encode())
 
+            
+            current_token_time = time.time()
+
                 # do HTTP POST to get token
             json_response_token = do_token_request(self, code)
             (access_token, token_type, expires_in, scope) = parse_json_response_token(json_response_token)
             #self.wfile.write(access_token.encode())
-
-            '''
-                # GESTIRE IL REFRESH DEL TOKEN QUANDO SCADE IL TIMEOUT (SUCCESSIVAMENTE)
-            '''
+            
+                # Gestisco il refresh del token, nel caso in cui sia scaduto
+            if (time.time() - current_token_time > expires_in):
+                json_response_token = do_refresh_token_request(self,code)
+                (access_token, token_type, expires_in, scope) = parse_json_response_token(json_response_token) 
             
                 # request for followed artists
             query_response = do_followed_artists_query(access_token)
